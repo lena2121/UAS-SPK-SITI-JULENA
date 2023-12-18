876f11f9c1a0808f7b2876d9f5fe111ef5c47bb4
lennalen <118107305+lena2121@users.noreply.github.com>
2023-12-18 14:49:41 +0700

    Add files via upload

---
+++ UAS21/README.md
@@ -0,0 +1,11 @@
+# Tugas UAS SPK
+Nama : Siti Julena <br>
+NIM : 201011401585 <br>
+Kelas : 07TPLP012 <br>
+
+## Install requirements
+```pip install -r requirements.txt```
+
+## Run the app
+Untuk menjalankan:
+```python main.py```
---
+++ UAS21/siti_SPK_Model/siti.SQl.sql
@@ -0,0 +1,11 @@
+INSERT INTO smartphonelenovo (brand,ram,prosesor,storage,baterai,harga,ukuran_layar) VALUES
+	 ('Lenovo C11','2 GB','MediaTek Helio G35','32 GB','5.000 mAh','Rp.1.499.000','6.5 inch'),
+	 ('Lenovo C15','4 GB','MediaTek Helio G35','64 GB','6.000 mAh','Rp.1.999.000','6.5 inch'),
+	 ('Lenovo 8 Pro','8 GB','Qualcomm Snapdragon 720G','128 GB','4.500 mAh','Rp.3.999.000','6.4 inch'),
+	 ('Lenovo X7 Pro','8 GB','MediaTek Dimensity 1000+','128 GB','4.500 mAh','Rp.4.999.000','6.55 inch'),
+	 ('Lenovo GT Neo','8 GB','MediaTek Dimensity 1200','128 GB','4.500 mAh','Rp.4.999.000','6.43 inch'),
+	 ('Lenovo X50 Pro','12 GB','Qualcomm Snapdragon 865','128 GB','4.200 mAh','Rp.7.999.000','6.44 inch'),
+	 ('Lenovo Narzo 50A','4 GB','MediaTek Helio G85','64 GB','6.000 mAh','Rp.2.499.000','6.5 inch'),
+	 ('Lenovo 9','8 GB','MediaTek Dimensity 810','128 GB','5.000 mAh','Rp.3.999.000','6.5 inch'),
+	 ('Lenovo X9 Pro','12 GB','Qualcomm Snapdragon 870','128 GB','4.500 mAh','Rp.5.999.000','6.55 inch'),
+	 ('Lenovo GT 2','12 GB','Qualcomm Snapdragon 898','128 GB','5.000 mAh','Rp.5.970.000','6.62 inch');
